6daf0bf498ffd63f8d95ce6118d25c85020c63bf
Fidel Agogo <35784821+fymagogo@users.noreply.github.com>
2023-03-21 17:01:54 +0000

    adding source for task 1

--- task1.js
+++ task1.js
@@ -1,1 +1,25 @@
+function longestWord(sentence) {
+  const words = sentence.toLowerCase().match(/[a-z]+/g); // Extracting all the words in the sentence into an array
+  let longest = '';
+    words.forEach(word => {
+        if (word.length > longest.length) { // If the current word is longer than the current longest word
+        longest = word;
+        } else if (word.length === longest.length) { // If the current word has the same length as the current longest word
+          if (countVowels(word) > countVowels(longest)) { // Choose the word with the highest number of vowels
+            longest = word;
+          }
+        }
+    })
+  return longest;
+}
 
+function countVowels(word) { // function to count vowels in a word
+  const vowels = "aeiou";
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i].toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
+}
